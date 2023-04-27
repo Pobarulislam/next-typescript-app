bb8bf94c2097a8ba2273a11737f73a47c11aaa96
Pobarulislam <pobarul.islam10@gmail.com>
2023-04-27 11:36:43 +0600

    news info typed on typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,25 @@
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Container } from 'react-bootstrap'
+import styles from "@/styles/app.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
-      <Component {...pageProps} />
+       <Head>
+        <title>NextJS News App</title>
+        <meta name="description" content="Pobarul islam" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container className={styles.pageContainer}>
+        <Component {...pageProps} />
+         </Container>
     </div>
   )
 }
